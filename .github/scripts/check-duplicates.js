c9a190536edfcc206514abe879da125010e8419b
LovelyJacob <57332486+jacobhumston@users.noreply.github.com>
2025-01-28 15:02:41 -0500

    Update check-duplicates.js

--- .github/scripts/check-duplicates.js
+++ .github/scripts/check-duplicates.js
@@ -3,7 +3,7 @@ const path = require('path');
 const jsonc = require('jsonc-parser');
 const { Octokit } = require('@octokit/rest');
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 const directoryPath = path.join(__dirname, '../lists');
